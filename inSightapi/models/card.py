016995171ce1fbf29de20a4985353571356c1901
Chaneice Jackson <c.jackson0126@gmail.com>
2022-08-31 23:40:12 -0500

    model issue fixed

--- inSightapi/models/card.py
+++ inSightapi/models/card.py
@@ -1,11 +1,12 @@
 from django.db import models
+from inSightapi.models.card_holder import Card_Holder
 
 class Card(models.Model):
-    card_user = models.ForeignKey('card_user', on_delete=models.CASCADE)
-    card_number = models.PositiveSmallIntegerField(max_length=16)
+    card_holder = models.ForeignKey('card_holder', on_delete=models.CASCADE)
+    card_number = models.PositiveSmallIntegerField()
     card_type = models.CharField(max_length=50)
-    expiration_date = models.PositiveSmallIntegerField(max_length=4)
-    security_code = models.PositiveSmallIntegerField(max_length=3)
+    expiration_date = models.PositiveSmallIntegerField()
+    security_code = models.PositiveSmallIntegerField()
     start_balance = models.DecimalField(max_digits=3, decimal_places=2)
     current_balance = models.DecimalField(max_digits=3, decimal_places=2)
     QRcode = models.CharField(max_length=500)
--- inSightapi/models/card_holder.py
+++ inSightapi/models/card_holder.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Card_User(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE ),
+class Card_Holder(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT),
     city = models.CharField(max_length=55)
     state = models.CharField(max_length=2)
     zip = models.PositiveSmallIntegerField()
-    card = models.ForeignKey("card", on_delete=models.CASCADE)
     
 
