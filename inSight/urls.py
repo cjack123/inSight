8785591b5822c973068ddfd228898691a5253038
Chaneice Jackson <c.jackson0126@gmail.com>
2022-09-03 13:11:02 -0500

    intial setup of auth, views, urls and tt

--- inSight/urls.py
+++ inSight/urls.py
@@ -14,8 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from inSightapi.views import register_user, login_user
+
+from rest_framework import routers
+from inSightapi.views import TransactionTypeView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'tranactiontypes', TransactionTypeView, 'tranactiontype')
+
 
 urlpatterns = [
+    path('register', register_user),
+    path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
+
---
+++ inSightapi/views/transaction_type.py
@@ -0,0 +1,38 @@
+"""View module for handling requests about transaction types"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from inSightapi.models import TransactionType, transaction_type
+
+
+class TransactionTypeView(ViewSet):
+    """Level up transaction types view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single transaction type
+
+        Returns:
+            Response -- JSON serialized game type
+        """
+        transaction_type = TransactionType.objects.get(pk=pk)
+        serializer = TransactionTypeSerializer(transaction_type)
+        return Response(serializer.data)
+        
+
+    def list(self, request):
+        """Handle GET requests to get all transaction types
+
+        Returns:
+            Response -- JSON serialized list of transaction types
+        """
+        transaction_types = TransactionType.objects.all()
+        serializer = TransactionTypeSerializer(transaction_types, many=True)
+        return Response(serializer.data)
+
+class TransactionTypeSerializer(serializers.ModelSerializer):
+    """JSON serializer for game types
+    """
+    class Meta:
+        model = TransactionType
+        fields = ('id', 'type')
